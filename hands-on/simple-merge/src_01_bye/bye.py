829dcb22fb965d9d3f8c91f2e89ef41a79243ab3
Geert Jan Bex <geertjan.bex@uhasselt.be>
2026-02-11 18:24:07 +0100

    Add source changes

---
+++ hands-on/simple-merge/src_01_bye/bye.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+import argparse
+
+
+def main():
+    arg_parser = argparse.ArgumentParser(
+            description="A simple hello world script"
+    )
+    arg_parser.add_argument("name", nargs="?", default="World")
+    args = arg_parser.parse_args()
+    print(f"Bye, {args.name}!")
+
+
+if __name__ == "__main__":
+    main()
